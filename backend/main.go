bc4651ffefd08a3e9a3384727535d264171e01b0
Haoran Su <Suhr08@bu.edu>
2025-02-27 13:58:59 -0500

    CORS added for login page, only frontend:3000 and future domain name can use

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/go-chi/cors"
+
+	"fmt"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -24,12 +29,19 @@ func main() {
 	log.Println("Environment variables loaded successfully")
 	port := os.Getenv("PORT")
 
+	frontEND_PORT := os.Getenv("FRONTEND_PORT")
+	log.Println("frontend port: ", frontEND_PORT)
+	domainName := os.Getenv("DomainName")
+	if domainName == "" {
+		domainName = ""
+	}
 	db := os.Getenv("DBURL")
 	r := chi.NewRouter()
 	server := &http.Server{
 		Addr:    port,
 		Handler: r,
 	}
+
 	// Initialize database
 	contxt, cancel := context.WithTimeout(context.Background(), 50*time.Second)
 	if err != nil {
@@ -44,10 +56,44 @@ func main() {
 	// Initialize routes
 
 	r.Use(middleware.Logger)
-	// main routes
+	// middleWare 
 	r.Use(Api.MiddleWare(client.Database("User")))
+	// Cors set up
+	r.Group(func(publicURL chi.Router) {
+		publicURL.Use(cors.Handler(
+			cors.Options{
+				AllowedOrigins:   []string{"*"}, // alllow any public urls
+				AllowedMethods:   []string{"GET", "POST","OPTIONS"},
+				AllowedHeaders:   []string{"Accept", "Content-Type", "Authorization"},
+				AllowCredentials: true,
+				MaxAge:           300,
+			},
+		))
+	})
+	r.Group(
+		func(privateURL chi.Router) {
+			privateURL.Use(cors.Handler(
+				cors.Options{
+					AllowedOrigins:   []string{fmt.Sprintf("https://%v.com", domainName), fmt.Sprintf("http://localhost:%v", frontEND_PORT)}, // alllow any public urls
+					AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE","OPTIONS"},
+					AllowedHeaders:   []string{"Accept", "Content-Type", "Authorization"},
+					AllowCredentials: true,
+					MaxAge:           300,
+				},
+			),
+			
+			)
+			privateURL.Options("/login", func(w http.ResponseWriter, r *http.Request) {
+		
+	})
+			privateURL.Post("/login", Api.LoginHandler)
+		},
+	)
 
-	r.Post("/login", Api.LoginHandler)
+	// main routes
+	
+
+	
 
 	log.Println("Server is running on port", port)
 
