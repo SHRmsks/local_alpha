f2dfef10c4ae7f9c7359b2275dbe0706c6bdebec
Haoran Su <Suhr08@bu.edu>
2025-03-01 00:05:26 -0500

    fixed

---
+++ frontend/src/app/dashBoard/page.js
@@ -0,0 +1,11 @@
+"use client"
+import Sidebar from "@/components/sidebar/sidebar";
+import "@/app/global.css"
+const DashBoard =  ()=>{
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+export default DashBoard
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,17 +1,40 @@
 "use client";
 import React, { useEffect } from "react";
+import Sidebar from "../components/sidebar/sidebar";
+import {
+  GoogleLogin,
+  GoogleOAuthProvider,
+  useGoogleLogin,
+} from "@react-oauth/google";
+import "./global.css";
+const GoogleloginButton = () => {
+  const login = useGoogleLogin({
+    flow: "auth-code",
+    ux_mode: "redirect",
+    redirect_uri: "http://localhost:5050/callback",
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+  });
+  return <button onClick={login}>Sign in with Google 🚀</button>;
+};
 export default function Home() {
   useEffect(() => {
-    const outcome = fetch("http://localhost:5050/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ UserName: "John", password: "123456" }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
+    // const outcome = fetch("http://localhost:5050/login", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ UserName: "John", password: "123456" }),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
+    //   .catch((err) => console.error(err));
   }, []);
-  return <div>Hello</div>;
+  return (
+    <div>
+    
+      <GoogleOAuthProvider clientId="70931151165-akujq6qnfukkn66heiuj51lfju7lvnod.apps.googleusercontent.com">
+        <GoogleloginButton />
+      </GoogleOAuthProvider>
+    </div>
+  );
 }
