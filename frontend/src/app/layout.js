53a64450211f424bd18508a04c7ca07c9454a65c
Augustin <augustinusmp@gmail.com>
2025-02-28 22:00:37 -0500

    moved header to page.js

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -9,10 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>
-                <Navbar></Navbar>
-                {children}
-            </body>
+            <body>{children}</body>
         </html>
     );
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,17 +1,18 @@
 "use client";
+import Navbar from "@/components/navbar/navbar";
 import React, { useEffect } from "react";
 export default function Home() {
-  useEffect(() => {
-    const outcome = fetch("http://localhost:5050/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ UserName: "John", password: "123456" }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
-  }, []);
-  return <div>Hello</div>;
+    useEffect(() => {
+        const outcome = fetch("http://localhost:5050/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ UserName: "John", password: "123456" }),
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+    }, []);
+    return <Navbar></Navbar>;
 }
