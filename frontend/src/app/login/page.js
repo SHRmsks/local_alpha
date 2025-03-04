e03cb3a56d10409cd65db2f6d4f8856d004600cb
Haoran Su <Suhr08@bu.edu>
2025-03-03 22:23:09 -0500

    fwf

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,12 +1,9 @@
 "use client";
 import React, { useEffect } from "react";
+import { useLinkedIn } from "react-linkedin-login-oauth2";
 
-import {
-
-  GoogleOAuthProvider,
-  useGoogleLogin,
-} from "@react-oauth/google";
-import "@/app/global.css"
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import "@/app/global.css";
 const GoogleloginButton = () => {
   const login = useGoogleLogin({
     flow: "auth-code",
@@ -16,6 +13,24 @@ const GoogleloginButton = () => {
   });
   return <button onClick={login}>Sign in with Google 🚀</button>;
 };
+
+const LinkedInButton =()=> {
+    const { linkedInLogin } = useLinkedIn({
+      clientId: "77nme6nzlhmnlv",
+      scope: "r_liteprofile r_emailaddress",
+      redirectUri: "http://localhost:5050/linkedin/callback",
+      onSuccess: (res) => {
+        console.log(res);
+      },
+      onError: (err) => {
+        console.error(err);
+      },
+    });
+    return <div onClick={linkedInLogin}>
+
+    login with LinkedIn
+    </div>
+}
 export default function Login() {
   useEffect(() => {
     // const outcome = fetch("http://localhost:5050/login", {
@@ -34,6 +49,8 @@ export default function Login() {
       <GoogleOAuthProvider clientId="70931151165-akujq6qnfukkn66heiuj51lfju7lvnod.apps.googleusercontent.com">
         <GoogleloginButton />
       </GoogleOAuthProvider>
+      
+      <LinkedInButton/>
     </div>
   );
 }
