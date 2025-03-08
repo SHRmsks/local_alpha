f230cb1328aa4c6c26eba8237556f353fa09f52b
Augustin <augustinusmp@gmail.com>
2025-03-07 18:35:20 -0800

    lg screens

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -7,17 +7,17 @@ import logoLinkedin from "@/../public/assets/logo-linkedin.svg";
 export default function LoginPage() {
     return (
         <div className="bg-iper-white w-full h-screen flex flex-col justify-center items-center">
-            <div className="bg-white rounded-[40px] p-8 md:p-10 w-80 md:w-96 h-fit shadow-md flex flex-col justify-center items-center gap-4">
+            <div className="bg-white rounded-[40px] p-8 w-80 md:w-96 lg:w-[448px] lg:h-[602px] h-fit shadow-md flex flex-col justify-center items-center gap-4 lg:gap-6">
                 <Image
                     src={logo}
                     alt="Iper Logo"
-                    className="size-10 md:size-14"
+                    className="size-10 md:size-14 lg:size-16 lg:mb-1"
                 />
-                <h1 className="font-krub font-extrabold text-2xl md:text-3xl">
+                <h1 className="font-krub font-extrabold text-2xl md:text-3xl lg:text-4xl">
                     Welcome to IPER
                 </h1>
                 <div className="w-full flex flex-col gap-1">
-                    <p className="text-sm md:text-base">EMAIL</p>
+                    <p className="text-sm">EMAIL</p>
                     <input
                         type="text"
                         placeholder="xxxxx@email.com"
@@ -25,7 +25,7 @@ export default function LoginPage() {
                     />
                 </div>
                 <div className="w-full flex flex-col gap-1">
-                    <p className="text-sm md:text-base">PASSWORD</p>
+                    <p className="text-sm">PASSWORD</p>
                     <input
                         type="text"
                         placeholder="Enter your password here"
@@ -41,11 +41,11 @@ export default function LoginPage() {
                     </button>
                 </div>
                 <div className="grid grid-cols-3 items-center my-[-6px]">
-                    <hr className="w-28 md:w-32 border-t border-iper-blue justify-self-start" />
+                    <hr className="w-28 md:w-32 lg:w-40 border-t border-iper-blue justify-self-start" />
                     <p className="text-xs md:text-sm text-iper-blue justify-self-center">
                         OR
                     </p>
-                    <hr className="w-28 md:w-32 border-t border-iper-blue justify-self-end" />
+                    <hr className="w-28 md:w-32 lg:w-40 border-t border-iper-blue justify-self-end" />
                 </div>
                 <div className="flex justify-center items-center gap-6 w-full">
                     <button className="flex items-center justify-center gap-2 text-[9px] md:text-[11px] h-9 md:h-10 px-3 bg-iper-white rounded-md border w-full">
