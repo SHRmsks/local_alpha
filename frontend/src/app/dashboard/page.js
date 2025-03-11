130c3048ae4fff64b0aaebb3f110b18358bd439a
Haoran Su <Suhr08@bu.edu>
2025-03-10 20:14:15 -0400

    linked with frontend

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -1,7 +1,12 @@
 "use client"
 import Sidebar from "@/components/sidebar/sidebar";
+import { useSearchParams } from "next/navigation";
 import "@/app/global.css"
+
+
+
 const Dashboard =  ()=>{
+const session_id = useSearchParams().get('session');
     return (
         <div>
             <Sidebar />
