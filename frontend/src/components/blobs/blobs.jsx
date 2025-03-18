1a4b48e3cf3c5c8a8179cf71f510f314717e3f3a
Augustin <augustinusmp@gmail.com>
2025-03-18 16:16:15 -0400

    changed path + width/height change

--- frontend/src/components/blobs/blobs.jsx
+++ frontend/src/components/blobs/blobs.jsx
@@ -2,41 +2,17 @@ import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { useEffect } from "react";
 
 export default function CustomPathMotion() {
-    const t = useMotionValue(0);
-    useEffect(() => {
-        const controls = animate(t, 360, {
-            duration: 20,
-            ease: "linear",
-            repeat: Infinity,
-            repeatType: "loop",
-        });
-        return controls.stop;
-    }, [t]);
-
-    const toRad = (deg) => (deg * Math.PI) / 180;
-
-    const A = 600,
-        B = 390;
-    const a = 3,
-        b = 2;
-    const delta = Math.PI / 2;
-
-    const x = useTransform(t, (v) => {
-        const rad = toRad(v);
-        return A * Math.sin(a * rad + delta);
-    });
-    const y = useTransform(t, (v) => {
-        const rad = toRad(v);
-        return B * Math.sin(b * rad);
-    });
-
     return (
         <motion.div
-            style={{
-                x,
-                y,
-            }}
             animate={{
+                x: [
+                    -800, -500, -200, 0, 200, 500, 800, 650, 800, 500, 200, 0,
+                    -200, -500, -800, -650, -800, -700, -800,
+                ],
+                y: [
+                    600, 550, 500, 400, 500, 450, 200, 0, -400, -600, -550,
+                    -500, -400, -500, -450, -200, 0, 400, 600,
+                ],
                 borderRadius: [
                     "100% 100% 100% 100% / 100% 100% 100% 100%",
                     "30% 70% 70% 30% / 30% 30% 70% 70%",
@@ -45,16 +21,36 @@ export default function CustomPathMotion() {
                     "60% 40% 30% 70% / 60% 30% 70% 40%",
                     "30% 70% 70% 30% / 30% 30% 70% 70%",
                 ],
+                width: [250, 600],
+                height: [600, 250],
             }}
             transition={{
+                default: {
+                    duration: 10,
+                    repeat: Infinity,
+                    ease: "easeInOut",
+                    repeatType: "mirror",
+                },
                 borderRadius: {
                     duration: 10,
                     repeat: Infinity,
                     ease: "easeInOut",
                     repeatType: "mirror",
                 },
+                x: {
+                    duration: 50,
+                    repeat: Infinity,
+                    ease: "linear",
+                    repeatType: "loop",
+                },
+                y: {
+                    duration: 50,
+                    repeat: Infinity,
+                    ease: "linear",
+                    repeatType: "loop",
+                },
             }}
-            className="absolute h-96 w-96 bg-iper-gold opacity-70"
+            className="absolute bg-iper-gold opacity-50"
         />
     );
 }
